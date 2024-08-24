cd84162b19398b154bcb5ff94c96a818b8f6bd47
Vignesh J <jvignesh609@gmail.com>
2024-08-24 17:16:14 +0530

    refact : changed the code for livewire nav | chnaged the route for home

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
     <div class="text-gray-800 font-semibold">
-        <span class="text-yellow-500 text-4xl">&lt;BWJ&gt;</span> Code
+        <span class="text-yellow-500 text-4xl">&lt;BWJ&gt;</span>
     </div>
 </a>
